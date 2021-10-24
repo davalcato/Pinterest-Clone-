92b13c9b1bf8afe66ba333a25f379aa704162163
davalcato <davalcato@yahoo.com>
2021-10-24 12:02:49 -0700

    first commit

---
+++ Pinterest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Pinterest
+//
+//  Created by Daval Cato on 10/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
