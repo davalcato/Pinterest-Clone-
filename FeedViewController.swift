2f7fd973d16097d0dcee50ba04ba0dcf29085a08
davalcato <davalcato@yahoo.com>
2021-11-13 13:00:24 -0800

    Created func named setupViewController on the MainTabBarController. Then added a new file named FeedViewController to create a navigation controller in the MainTabBarController

---
+++ FeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FeedViewController.swift
+//  Pinterest
+//
+//  Created by Daval Cato on 11/13/21.
+//
+
+import UIKit
+
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+}
+
--- Pinterest/MainTabBarController.swift
+++ Pinterest/MainTabBarController.swift
@@ -16,6 +16,29 @@ class MainTabBarController: UITabBarController {
         
         tabBar.barTintColor = UIColor.white
         tabBar.clipsToBounds = true
+        
+        setupViewController()
+    }
+    
+    func setupViewController() {
+        // for each the controller displayed
+        let feedNavController = UINavigationController(rootViewController: FeedViewController())
+        feedNavController.tabBarItem.title = "Home"
+        feedNavController.tabBarItem.image = UIImage(named: "logo-unselected-tab")
+        
+        // so withRenderingMode doesn't overlay a color on the image
+        feedNavController.tabBarItem.selectedImage = UIImage(named: "logo-tab")?.withRenderingMode(.alwaysOriginal)
+        // title color
+        feedNavController.tabBarItem.setTitleTextAttributes([
+            // set the ns string
+            NSAttributedString.Key.foregroundColor: UIColor.gray
+        // when select title color is gray
+        ], for: .selected)
+        // setup viewcontrollers
+        viewControllers = [
+            feedNavController
+        
+        ]
     }
 }
 
