0646f17ef4345c44d43cf493c4cc913c13c3e72f
davalcato <davalcato@yahoo.com>
2021-11-19 12:55:02 -0800

    Added the chat button onto the right corner of the FeedViewController

--- Pinterest/FeedViewController.swift
+++ Pinterest/FeedViewController.swift
@@ -21,6 +21,15 @@ class FeedViewController: UIViewController {
         
     }()
     
+    // add chat button
+    let chatBtn: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "chatbubble"), for: .normal)
+        
+        return btn
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,12 +43,27 @@ class FeedViewController: UIViewController {
         
         // add search as subview
         navBar.addSubview(searchTF)
+        navBar.addSubview(chatBtn)
+        
+        chatBtn.constraint(
+            top: nil,
+            left: nil,
+            bottom: nil,
+            right: navBar.rightAnchor,
+            topConstant: 0,
+            leftConstant: 0,
+            bottomConstant: 5,
+            rightConstant: 18,
+            widthConstant: 25,
+            heightConstant: 25)
+        chatBtn.centerYAnchor.constraint(equalTo: navBar.centerYAnchor).isActive = true
+        
         
         searchTF.constraint(
             top: navBar.topAnchor,
             left: navBar.leftAnchor,
             bottom: navBar.bottomAnchor,
-            right: navBar.rightAnchor,
+            right: chatBtn.leftAnchor,
             topConstant: 5,
             leftConstant: 10,
             bottomConstant: 5,
