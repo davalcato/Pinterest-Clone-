adf8ccdec479eac317ce31a8270bc36e5031b5c4
davalcato <davalcato@yahoo.com>
2021-12-11 12:22:13 -0800

    added the layoutAttributesForElements to the PinterestLayout file and used visiblelayoutAttributes to loop thru the cached attributes.

--- Pinterest/PinterestLayout.swift
+++ Pinterest/PinterestLayout.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
+protocol PinterestLayoutDelegate: class {
+    // delegate method for the feedview
+    func collectionView(_ collectionView: UICollectionView, heightForImageAtIndexPath indexPath: IndexPath)
+    -> CGFloat
+    
+}
+
 // define the class
 class PinterestLayout: UICollectionViewLayout {
+    
+    weak var delegate: PinterestLayoutDelegate!
     // define the properties
     var numberOfColumns = 2
-    var cellPadding = 5
+    var cellPadding: CGFloat = 5
     // array of
-    var cashedAttributes = [UICollectionViewLayoutAttributes]()
+    var cachedAttributes = [UICollectionViewLayoutAttributes]()
     
     var contentHeight: CGFloat = 0
     
@@ -29,7 +38,7 @@ class PinterestLayout: UICollectionViewLayout {
     
     override func prepare() {
         // check if empty -- then make sure a collectionView is passed in
-        guard cashedAttributes.isEmpty == true, let collectionView = collectionView else { return }
+        guard cachedAttributes.isEmpty == true, let collectionView = collectionView else { return }
         
         let columnWidth = collectionViewContentSize.width / CGFloat(numberOfColumns)
         var xPosition = [CGFloat]()
@@ -40,11 +49,54 @@ class PinterestLayout: UICollectionViewLayout {
         var yPosition = [CGFloat](repeating: 0, count: numberOfColumns)
         
         // loop thru all this items in the collection
-        for item in 0..< collectionView.numberOfItems(inSection: 0) {
+        for item in 0 ..< collectionView.numberOfItems(inSection: 0) {
             let indexPath = IndexPath(item: item, section: 0)
             
+            // calculate the frame of cell
+            let photoHeight = delegate.collectionView(collectionView, heightForImageAtIndexPath: indexPath)
+            // calculate the height
+            let height = cellPadding * 2 + photoHeight
+            // frame
+            let frame = CGRect(
+                x: xPosition[column],
+                y: yPosition[column],
+                width: columnWidth,
+                height: height)
+            
+            let insetFrame = frame.insetBy(dx: cellPadding, dy: cellPadding)
+            
+            let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+            attributes.frame = insetFrame
+            // append
+            cachedAttributes.append(attributes)
+            
+            contentHeight = max(contentHeight, frame.maxY)
+            
+            // advance the y position offset
+            yPosition[column] = yPosition[column] + height
+            
+            column = column < (numberOfColumns - 1) ? (column + 1) : 0
         }
     }
+    // method
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        // calculate the visiblelayout
+        var visiblelayoutAttributes = [UICollectionViewLayoutAttributes]()
+        // loop thru the cached attributes
+        for attributes in cachedAttributes {
+            // if frame is correct append it to the array
+            if attributes.frame.intersects(rect) {
+                visiblelayoutAttributes.append(attributes)
+            }
+        }
+        
+        return visiblelayoutAttributes
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        return cachedAttributes[indexPath.item]
+    }
+    
 }
 
 
