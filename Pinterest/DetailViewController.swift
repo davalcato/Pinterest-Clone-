ad4c797a1b4cd5c85d32f4687174a7aeeccd04a9
davalcato <davalcato@yahoo.com>
2021-12-11 16:15:19 -0800

    created a back button on the DetailViewController on the Zoom in feature and the name is backBtn.

--- Pinterest/DetailViewController.swift
+++ Pinterest/DetailViewController.swift
@@ -13,16 +13,49 @@ class DetailViewController: UIViewController {
     var post: Post? {
         didSet {
             guard let p = post else { return }
-            
         }
-        
     }
+    
+    let backBtn: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "back"), for: .normal)
+        btn.contentEdgeInsets = UIEdgeInsets(
+            top: 8,
+            left: 8,
+            bottom: 8,
+            right: 8)
+        btn.addTarget(self, action: #selector(backBtnTapped), for: .touchUpInside)
+        
+        return btn
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        // constrain back button
+        view.addSubview(backBtn)
+        
+        backBtn.constraint(
+            top: view.topAnchor,
+            left: view.leftAnchor,
+            bottom: nil,
+            right: nil,
+            topConstant: 40,
+            leftConstant: 18,
+            bottomConstant: 0,
+            rightConstant: 0,
+            widthConstant: 38,
+            heightConstant: 38)
+        
     }
+    // add the selector method
+    @objc func backBtnTapped() {
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
 }
 
 
