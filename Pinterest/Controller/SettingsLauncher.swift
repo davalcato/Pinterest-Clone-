ea2a8d769e6eb752a33b4f8a620e93312354580d
davalcato <davalcato@yahoo.com>
2022-05-05 13:53:13 -0700

    added an acceleration of the animation with ease in and out by adding animate withDuration onto the SettingsLauncher

--- Pinterest/Controller/SettingsLauncher.swift
+++ Pinterest/Controller/SettingsLauncher.swift
@@ -20,15 +20,15 @@ class SettingsLauncher: NSObject {
         
     }()
     
-     func showSettings() {
+    func showSettings() {
         if let window = UIApplication.shared.windows.first {
-        
-        blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
-        // dismiss blackview
-        blackView.addGestureRecognizer(UITapGestureRecognizer(
-            target: self,
-            action: #selector(handleDismiss)))
-        
+            
+            blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+            // dismiss blackview
+            blackView.addGestureRecognizer(UITapGestureRecognizer(
+                target: self,
+                action: #selector(handleDismiss)))
+            
             // Add has a subview to viewController view
             window.addSubview(blackView)
             window.addSubview(collectionView)
@@ -45,21 +45,27 @@ class SettingsLauncher: NSObject {
             
             
             blackView.frame = window.frame
-        // this calls blackview to screen
-        blackView.alpha = 0
-        // animate the black view onto screen
-        UIView.animate(
-            withDuration: 0.5) {
-            self.blackView.alpha = 1
-            // setting the new frame of collectionView to animate
-            self.collectionView.frame = CGRect(
-                x: 0,
-                y: y,
-                width: self.collectionView.frame.width,
-                height: self.collectionView.frame.height)
+            // this calls blackview to screen
+            blackView.alpha = 0
             
-            
-            }
+            // change duration of animation
+            UIView.animate(
+                withDuration: 0.5,
+                delay: 0,
+                usingSpringWithDamping: 1,
+                initialSpringVelocity: 1,
+                options: .curveEaseOut,
+                animations: {
+                
+                self.blackView.alpha = 1
+                // setting the new frame of collectionView to animate
+                self.collectionView.frame = CGRect(
+                    x: 0,
+                    y: y,
+                    width: self.collectionView.frame.width,
+                    height: self.collectionView.frame.height)
+            },
+                completion: nil)
             print("Button tapped")
         }
     }
@@ -73,9 +79,9 @@ class SettingsLauncher: NSObject {
     override init() {
         super.init()
         
-//        collectionView.dataSource = self
-//        collectionView.delegate = self
-//
-//        collectionView.register(SettingCell.self, forCellWithReuseIdentifier: cellId)
+        //        collectionView.dataSource = self
+        //        collectionView.delegate = self
+        //
+        //        collectionView.register(SettingCell.self, forCellWithReuseIdentifier: cellId)
     }
 }
